06eb216d6cb94c66a238e4d9ee5f80ba01bbfe2c
Madcoder8 <amdsuxalot@yahoo.com>
2015-01-21 07:59:19 -0800

    Adding new material and light movement

---
+++ Assets/LightLookAt.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightLookAt : MonoBehaviour {
+
+    Vector3 Target;
+    public GameObject TargetObject;
+    public Camera TargetCamera;
+    public int MoveSpeed = 10;
+    public Light ColorLight;
+    //public Color Test;
+    //public float TestX;
+    //public float TestY;
+
+    public void MoveLookAt(float x, float y){
+
+        Vector3 TempTarget = Target + new Vector3(MoveSpeed * x * Time.deltaTime, MoveSpeed * y * Time.deltaTime, 0);
+        Vector3 viewPos = TargetCamera.camera.WorldToViewportPoint(TempTarget);
+        //check for outside of viewport, don't update if it happens
+        if (viewPos.x < 0.0f || viewPos.y < 0.0f || viewPos.x > 1.0f || viewPos.y > 1.0f)
+        {
+            return;
+        }
+
+        //inside viewport so update target
+        Target = TempTarget;
+
+    }
+
+    public void ChangeLightColor( Color newColor ){
+
+        ColorLight.color = newColor;
+    }
+
+	// Use this for initialization
+	void Start () {
+        Target = TargetObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(Target);
+        //ChangeLightColor(Test);
+        //MoveLookAt(TestX, TestY);
+	}
+}
