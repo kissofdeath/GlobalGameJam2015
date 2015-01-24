e80e758c6b118e69aa95bb9c9dafca99c26873ca
Madcoder8 <amdsuxalot@yahoo.com>
2015-01-24 15:21:09 -0800

    treasure box

---
+++ Assets/Chesty.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chesty : MonoBehaviour {
+
+    enum ToggleState { OPEN_STATE, CLOSED_STATE };
+    ToggleState CurrentState;
+    int OpenState;
+    int CloseState;
+    private Animator anim;
+    public bool TestBool = false;
+
+	// Use this for initialization
+	void Start () {
+
+        CurrentState = ToggleState.CLOSED_STATE;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (TestBool)
+        {
+            ToggleChest();
+            TestBool = false;
+        }
+            
+	}
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        OpenState = Animator.StringToHash("Base Layer.box_open");
+        CloseState = Animator.StringToHash("Base Layer.box_close");      
+    }
+
+    public void ToggleChest()
+    {
+        switch (CurrentState)
+        {
+            case ToggleState.CLOSED_STATE:
+                CurrentState = ToggleState.OPEN_STATE;
+                anim.Play(OpenState);
+                break;
+            case ToggleState.OPEN_STATE:
+                CurrentState = ToggleState.CLOSED_STATE;
+                anim.Play(CloseState);
+                break;
+        }
+        
+    }
+
+}
