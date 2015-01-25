1430238ad4a32e459477068d7054de2e5c960858
kissofdeath <meetraja23@gmail.com>
2015-01-25 13:02:47 -0800

    Shield stuff - visually works.

---
+++ Assets/Scripts/Effects/Shield.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shield : MonoBehaviour {
+    public float shieldDuration = 3f;
+
+    void OnEnable()
+    {
+        Invoke("KillMe", shieldDuration);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void KillMe()
+    {
+        Destroy(this.gameObject);
+    }
+
+}
