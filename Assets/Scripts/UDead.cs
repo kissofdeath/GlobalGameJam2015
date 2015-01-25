b4e2882f7fe3fd138a45de97f8c235407b5f57b0
Madcoder8 <amdsuxalot@yahoo.com>
2015-01-25 11:02:47 -0800

    added deadzone

---
+++ Assets/Scripts/UDead.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UDead : MonoBehaviour {
+
+    public Player Klin;
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log("U Dead!");
+        PikeColliderListener DeadBoy = other.GetComponent<PikeColliderListener>();
+
+        if (DeadBoy != null && Klin != null)
+        {
+            Klin.Respawn();
+        }
+    }
+}
