969bd7674c32b9fefd23b4bc6a1192852d4c6b94
Madcoder8 <amdsuxalot@yahoo.com>
2015-01-25 09:11:54 -0800

    adding some bullet behavior

---
+++ Assets/BulletMove.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletMove : MonoBehaviour {
+
+    public Transform target;
+    public float speed = 10f;
+    private float StartTime;
+    public float SelfDestructTime = 3f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnEnable()
+    {
+        StartTime = Time.time;
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        //check to see if we have flown long enough
+        if( Time.time - StartTime >= SelfDestructTime)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+
+        //continue to your target
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+	
+	}
+}
---
+++ Assets/BulletPool.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletPool : MonoBehaviour {
+
+    int ArraySize = 500;
+    GameObject[] BulletArray;
+    GameObject MasterBullet;
+
+	// Use this for initialization
+	void Start () {
+	    BulletArray = new GameObject[ArraySize];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
